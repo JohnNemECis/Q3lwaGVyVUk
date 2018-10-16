888afe7cb203cce5873c3da182ddac58332feefc
JohnNemECis <31183873+JohnNemECis@users.noreply.github.com>
2018-10-17 01:03:08 +0200

    Add files via upload
    So, Here it is.

---
+++ CypherUI/CypherUI/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  CypherUI
+//
+//  Created by Team CypherDevelopment Remote Desktop on 18/09/2018.
+//  Copyright © 2018 CyDev. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
---
+++ CypherUI/CypherUI/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  CypherUI
+//
+//  Created by Team CypherDevelopment Remote Desktop on 18/09/2018.
+//  Copyright © 2018 CyDev. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+   
+    }
+    
+}
